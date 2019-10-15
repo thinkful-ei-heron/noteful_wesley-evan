bb8145f662b11ebf7fff647cdf200eff0b0ef087
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-10-15 14:01:11 -0700

    fully functional assignment

--- src/SideBar/GoBackButton.js
+++ src/SideBar/GoBackButton.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 export class GoBackButton extends Component {
   render() {
     return (
       <div>
-        <button>Go back</button>
+        <button><Link to="/">Go back</Link></button>
       </div>
     )
   }
